3e629082d3b361cd5b9a00dce7b2f450f9afece1
alliarnold <89819872+alliarnold@users.noreply.github.com>
2022-02-16 19:32:29 -0500

    Updated onclick and JS function

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const label = document.getElementById("name-label")
+const input = document.getElementById("name-input")
+const button = document.getElementById("name-submit")
+
+let userName;
+
+function updateName() {
+    userName = input.value
+    console.log(userName)
+    label.innerText = "Wow! I love the name " + userName
+    button.innerText = "Try again!"
+    input.value = ""
+}
