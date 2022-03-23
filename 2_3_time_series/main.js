e0cd815f698c19f92f87e9c9a715e584a73ab7da
alliarnold <89819872+alliarnold@users.noreply.github.com>
2022-03-23 12:20:12 -0400

    Added Variable - SVG

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,25 +1,40 @@
- /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ;
+// VARIABLES
+const width = window.innerWidth,
+  height = window.innerHeight,
+  margin = 30;
 
-/* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d => {
+// DATA
+d3.csv('../data/populationOverTime.csv', d => {
   return {
-    // year: new Date(+d.Year, 0, 1),
-    // country: d.Entity,
-    // population: +d.Population
+    /* property */ year: new Date(+d.Year, 0, 1), /*explaining how to interpret a year month numbers in java go 0 - 11, 0, 1 = Jan 1*/
+    /* property */ country: d.Entity,
+    /* property */ population: +d.Population
   }
 }).then(data => {
   console.log('data :>> ', data);
 
   // SCALES
+    const xScale = d3.scaleTime()
+      .domain(d3.extent(data, d => d.year))
+      .range([margin, width - margin])
+
+    const yScale = d3.scaleLinear
+      .domain(d3.extent(data, d =>  d.population))
+      .range([height - margin, margin])
 
   // CREATE SVG ELEMENT
+
+    const aSVG = d3.select("#container")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+
 
   // BUILD AND CALL AXES
 
   // LINE GENERATOR FUNCTION
+
+    const lineGen = d3.
 
   // DRAW LINE
 
