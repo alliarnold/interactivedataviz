6898a3dc3f03827ee857850ce86eaf1984531150
alliarnold <89819872+alliarnold@users.noreply.github.com>
2022-04-10 21:02:43 -0400

    work-on-3.1

--- 3_1_quantities_and_amounts/main.js
+++ 3_1_quantities_and_amounts/main.js
@@ -1,19 +1,27 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * .7,
+  height = window.innerHeight * .7,
+  margin = { top: 20, bottom: 60, left: 60, right: 20},
+  formatDate = d3.timeFormat("%Y");
 
 // // since we use our scales in multiple functions, they need global scope
-// let xScale, yScale;
+let svg, xScale, yScale, colorScale;
 
 /* APPLICATION STATE */
 let state = {
-  // data: [],
+  data: [],
+  selectedCategory: "up9Units"
 };
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
+d3.json("../data/CompostingPickups.json", d => {
+  // use custom initializer to reformat the data the way we want it
+  return {
+    Year: new Date(+d.Year),
+    Type: d.Type,
+    Count: d.Count
+  };
+}).then(raw_data => {
   console.log("data", raw_data);
   // save our data to application state
   state.data = raw_data;
@@ -24,8 +32,70 @@ d3.csv('[PATH_TO_YOUR_DATA]', d3.autoType).then(raw_data => {
 // this will be run *one time* when the data finishes loading in
 function init() {
   /* SCALES */
+  xScale = d3.scaleBand()
+    .domain(state.data.map(d => d.Year))
+    .range([0, width])
+    .paddingInner(.2)
 
+  yScale = d3.scaleLinear()
+    .domain([0, d3.max(state.data, d => d.Count)])
+    .range([height, 0])
 
+  colorScale = d3.scaleOrdinal()
+    .domain("up9Units", "10upUnits", "schools")
+    .range(["red", "orange", "blue"])
+
+  const xAxis = d3.axisBottom(xScale)
+  const yAxis = d3.axisLeft(yScale)
+
+  const selectElement = d3.select("#dropdown")
+  selectElement
+    .selectAll("option")
+    .data([
+      {key: "up9Units", label: "Buildings with 1-9 Units"},
+      {key: "10upUnits", label: "Buildings with 10+ Units"},
+      {key: "schools", label: "Schools"}])
+    .join("option")
+    .attr("value", d => d.key)
+    .text(d => d.label)
+  
+  selectElement.on("change", event => {
+    console.log("DROPDOWN CALLBACK: new value is", event.target.value);
+    state.selectedCategory = event.target.value;
+    console.log("NEW STATE:", state);
+    draw();
+  });
+
+  mySvg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  
+  const xAxisGroup = svg.append("g")
+    .attr("class", 'xAxis')
+    .attr("transform", `translate(${margin.left}, ${height-18})`)
+    .call(xAxis)
+
+  const yAxisGroup = svg.append("g")
+    .attr("class", 'yAxis')
+    .attr("transform", `translate(${60}, ${-18})`)
+    .call(yAxis)
+
+  xAxisGroup.append("text")
+    .attr("class", 'axis-title')
+    .attr("x", width/4)
+    .attr("y", 20)
+    .attr("writing-mode", "vertical-lr")
+    .attr("text-anchor", "middle")
+    .text("Year")
+  
+  yAxisGroup.append("text")
+    .attr("class", 'axis-title')
+    .attr("x", -20)
+    .attr("y", height/4)
+    .attr("writing-mode", "verticle-lr")
+    .attr("text-anchor", "middle")
+    .text("Number of Participants")
 
   draw(); // calls the draw function
 }
@@ -33,8 +103,51 @@ function init() {
 /* DRAW FUNCTION */
 // we call this every time there is an update to the data/state
 function draw() {
-  /* HTML ELEMENTS */
- 
+
+  mySvg.selectAll("rect")
+    .data(state.data)
+    .join("rect")
+    .attr("height", d => yScale(state.data, d.Count))
+    .attr("width", xScale.bandwidth())
+    .attr("x", d => xScale(state.data, d.Year))
+    .attr("y", 0)
+    .attr("fill", d => colorScale(state.data, d.Type));
+
+ /*
+const filteredData = state.data
+  .filter(d => state.selectedCategory === "up9Units" || state.selectedCategory === d.Type )
+
+const bars = svg
+  .selectAll("rect")
+  .data(filteredData)
+    .join(
+      enter => enter.append("rect")
+      .attr("width", xScale.bandwidth())
+      .attr("height", d => yScale(state.data, d.Count))
+      .attr("fill", d => colorScale(state.data, d.Type))
+      .attr("x", 0)
+      .attr("y", 0)
+      .call(sel => sel.transition()
+        .duration(250)
+        .attr("x", d=> xScale(state.data, d.Year))
+        ),
+
+      update => update
+        .call(sel => sel
+          .transition()
+          .duration(250)
+        ),
+
+      exit => exit
+          .call(sel => sel
+            .attr("opacity", 1)
+            .transition()
+            .duration(250)
+            .attr("opacity", 0)
+            .remove()
+          )
+    ); */
 
 
-}
+
+};
---
+++ 4_1_geographic_map/main.js
@@ -0,0 +1,74 @@
+
+// FIRST Set up initial map center and zoom level
+// relatively central coordinates for NYC: 40.70748615030739, -73.92744598608154
+// zoom levels get closer the higher the number - try 11 or 12 or 13 to start
+
+const map = L.map('map', {
+    center: [40.70748615030739, -73.92744598608154],
+    zoom: 12
+});
+
+let controlLayers;
+
+controlLayers  = L.control.layers(null, null, {
+    position: "topright", 
+    collapsed: false
+}).addTo(map);
+
+/*
+Later in process: Add a legend (checkboxes) to the upper-right corner.
+At first, baselayers and overlays are set to `null` (empty legend).
+We will be adding items to the legend as we load each layer.
+*/  
+
+    
+// display basemap tiles 
+
+const baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// see more basemap options at https://leaflet-extras.github.io/leaflet-providers/preview/
+
+
+// read data from data files
+
+const wataerAvg2020 = $.getJSON('../data/2020s-Mean-Monthly-High-Water.geojson', function(data){
+    let overlay2020 = L.geoJson(data)
+    overlay2020.addTo(map);
+    controlLayers.addOverlay(overlay2020, "Water High Average 2020")
+});
+
+const wataerAvg2050 = $.getJSON('../data/2050s-Mean-Monthly-High-Water.geojson', function(data){
+    let overlay2050 = L.geoJson(data)
+    overlay2050.addTo(map);
+    controlLayers.addOverlay(overlay2050, "Water High Average 2050")
+});
+
+const wataerAvg2080 = $.getJSON('../data/2050s-Mean-Monthly-High-Water.geojson', function(data){
+    let overlay2080 = L.geoJson(data)
+    overlay2080.addTo(map);
+    controlLayers.addOverlay(overlay2080, "Water High Average 2080")
+});
+
+let accessPlans = $.getJSON('../data/Waterfront-Access-Plans.geojson', function(data){
+    let overlayPlans = L.geoJson(data, {
+        style: function (feature) {
+            return {color: "orange"}
+        },
+        onEachFeature: function (feature, layer) {layer.bindPopup(feature.properties.name);
+        }
+    })
+    overlayPlans.addTo(map);
+    controlLayers.addOverlay(overlayPlans, "Waterfront Access Plan");
+});
+
+const myCircle01 = L.circle([40.70748615030739, -73.92744598608154], {
+    color: 'magenta',
+    fillColor: 'oragne',
+    radius: 500,
+}).addTo(map);
+
+myCircle01.bindPopup("area of interest")
+// add data overlay layers
